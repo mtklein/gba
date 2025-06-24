86b8a71a4afc91668f489d51fddc3798c166eb27
Mike Klein <mtklein@gmail.com>
2025-06-24 18:49:52 +0800

    personal style cleanup

--- src/main.c
+++ src/main.c
@@ -1,120 +1,123 @@
 #include <stdint.h>
 
-#define REG_DISPCNT (*(volatile uint16_t*)0x4000000)
+#define REG_DISPCNT (*(uint16_t volatile*)0x4000000)
+#define REG_VCOUNT  (*(uint16_t volatile*)0x4000006)
 #define MODE4 4
 #define BG2_ENABLE (1<<10)
-#define PALETTE ((volatile uint16_t*)0x5000000)
-#define REG_VCOUNT (*(volatile uint16_t*)0x4000006)
 
-#define SCREEN_WIDTH 240
-#define SCREEN_HEIGHT 160
+#define W 240
+#define H 160
 
-#define COLOR_BLACK 0
-#define COLOR_WHITE 1
+static uint16_t* swap_buffers(uint16_t *front, uint16_t *back) {
+    while (REG_VCOUNT >= H);
+    while (REG_VCOUNT <  H);
 
-#define FRONT_BUFFER ((volatile uint16_t*)0x6000000)
-#define BACK_BUFFER  ((volatile uint16_t*)0x600A000)
-
-static volatile uint16_t* videoBuffer = BACK_BUFFER;
-
-static inline void flipPage(void) {
-    if(REG_DISPCNT & (1<<4)) {
-        REG_DISPCNT &= ~(1<<4);
-        videoBuffer = BACK_BUFFER;
+    REG_DISPCNT ^= (1<<4);
+    if (REG_DISPCNT & (1<<4)) {
+        return front;
     } else {
-        REG_DISPCNT |= (1<<4);
-        videoBuffer = FRONT_BUFFER;
+        return back;
     }
 }
 
-static inline void waitForVBlank(void) {
-    while(REG_VCOUNT >= 160);
-    while(REG_VCOUNT < 160);
-}
+union rgb555 {
+    uint16_t rgbx;
+    struct {
+        uint16_t r : 5;
+        uint16_t g : 5;
+        uint16_t b : 5;
+        uint16_t x : 1;
+    };
+};
 
-static inline void drawPixel(int x, int y, uint8_t color) {
-    uint32_t idx;
-    uint16_t cur;
-    if(x < 0 || x >= SCREEN_WIDTH || y < 0 || y >= SCREEN_HEIGHT) return;
-    idx = (uint32_t)y * (SCREEN_WIDTH/2u) + (uint32_t)(x >> 1);
-    cur = videoBuffer[idx];
-    if(x & 1) {
-        uint16_t mask = (uint16_t)((uint16_t)color << 8);
-        cur = (uint16_t)((cur & 0x00FFu) | mask);
+union mode4_pair {
+    uint16_t both;
+    struct { uint8_t lo,hi; };
+};
+
+static inline void set_pixel(uint16_t *fb,
+                             int x, int y, uint8_t color) {
+    int const ix = y * (W/2) + x/2;
+    union mode4_pair px = {fb[ix]};
+    if (x & 1) {
+        px.hi = color;
     } else {
-        uint16_t mask = (uint16_t)color;
-        cur = (uint16_t)((cur & 0xFF00u) | mask);
+        px.lo = color;
     }
-    videoBuffer[idx] = cur;
+    fb[ix] = px.both;
 }
 
-static void fillRect(int x, int y, int w, int h, uint8_t color) {
-    for(int r = 0; r < h; ++r)
-        for(int c = 0; c < w; ++c)
-            drawPixel(x + c, y + r, color);
+static void fill_rect(uint16_t *fb,
+                      int l, int t, int w, int h, uint8_t color) {
+    int const r = l+w,
+              b = t+h;
+    for (int y = t; y < b; y++)
+    for (int x = l; x < r; x++) {
+        set_pixel(fb, x,y, color);
+    }
 }
 
-static void clearScreen(uint8_t color) {
-    uint16_t fill = (uint16_t)(color | (color << 8));
-    for(int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT / 2; ++i)
-        videoBuffer[i] = fill;
+static void clear(uint16_t *fb, uint8_t color) {
+    union mode4_pair const fill = {.lo=color, .hi=color};
+    for(int i = 0; i < W*H/2; i++) {
+        fb[i] = fill.both;
+    }
 }
 
-typedef struct {
-    int x, y;  // fixed point 8.8
-    int vx, vy;
-} Ball;
-
-#define BALL_SIZE 8
-#define BALL_SPEED 256
+struct ball {
+    int x,vx,  // All .8 fixed point
+        y,vy;
+};
 
 int main(void) {
-    Ball ball;
-
     REG_DISPCNT = MODE4 | BG2_ENABLE;
-    PALETTE[COLOR_BLACK] = 0;
-    PALETTE[COLOR_WHITE] = 0x7FFF;
 
-    videoBuffer = FRONT_BUFFER;
-    clearScreen(COLOR_BLACK);
-    videoBuffer = BACK_BUFFER;
-    clearScreen(COLOR_BLACK);
+    uint16_t *palette = (uint16_t*)0x05000000,
+             *front   = (uint16_t*)0x06000000,
+             *back    = (uint16_t*)0x0600A000,
+             *fb      = back;
 
-    ball.x = (SCREEN_WIDTH/2 - BALL_SIZE/2) << 8;
-    ball.y = (SCREEN_HEIGHT/2 - BALL_SIZE/2) << 8;
-    ball.vx = BALL_SPEED;
-    ball.vy = BALL_SPEED;
+    enum {BG,BALL};
+    palette[  BG] = ((union rgb555){.r=31, .g=31, .b=31}).rgbx;
+    palette[BALL] = ((union rgb555){.r= 0, .g=15, .b=31}).rgbx;
 
-    while(1) {
-        int bx;
-        int by;
+    clear(front, BG);
+    clear( back, BG);
+
+    int const size = 6, speed = 512;
+
+    struct ball ball = {
+        .x = (W/2 - size/2) << 8, .vx = speed,
+        .y = (H/2 - size/2) << 8, .vy = speed,
+    };
+
+    while (1) {
+        fb = swap_buffers(front, back);
 
         ball.x += ball.vx;
         ball.y += ball.vy;
 
-        bx = ball.x >> 8;
-        by = ball.y >> 8;
+        int const ix = ball.x >> 8,
+                  iy = ball.y >> 8;
 
-        if(bx <= 0) {
-            ball.vx = BALL_SPEED;
-            ball.x = 0;
-        } else if(bx >= SCREEN_WIDTH - BALL_SIZE) {
-            ball.vx = -BALL_SPEED;
-            ball.x = (SCREEN_WIDTH - BALL_SIZE) << 8;
+        clear    (fb, BG);
+        fill_rect(fb, ix,iy, size,size, BALL);
+
+        if (ix <= 0) {
+            ball. x = 0;
+            ball.vx = +speed;
+        } else if (ix >= W - size) {
+            ball. x = (W - size) << 8;
+            ball.vx = -speed;
         }
 
-        if(by <= 0) {
-            ball.vy = BALL_SPEED;
-            ball.y = 0;
-        } else if(by >= SCREEN_HEIGHT - BALL_SIZE) {
-            ball.vy = -BALL_SPEED;
-            ball.y = (SCREEN_HEIGHT - BALL_SIZE) << 8;
+        if (iy <= 0) {
+            ball. y = 0;
+            ball.vy = +speed;
+        } else if (iy >= H - size) {
+            ball. y = (H - size) << 8;
+            ball.vy = -speed;
         }
-
-        waitForVBlank();
-        flipPage();
-        clearScreen(COLOR_BLACK);
-        fillRect(ball.x >> 8, ball.y >> 8, BALL_SIZE, BALL_SIZE, COLOR_WHITE);
     }
 }
 
