594f22bbfa1256218afb2acc3f087e1cd4d97bfe
Mike Klein <mtklein@gmail.com>
2025-06-27 13:14:46 +0800

    Add __aeabi_memcpy implementation

--- src/crt0.s
+++ src/crt0.s
@@ -5,3 +5,18 @@ _start:
     ldr r0, =main
     bx  r0
 1:  b 1b
+
+    .thumb_func
+    .global __aeabi_memcpy
+__aeabi_memcpy:
+    cmp r2, #0
+    beq 2f
+1:
+    ldrb r3, [r1]
+    adds r1, #1
+    strb r3, [r0]
+    adds r0, #1
+    subs r2, #1
+    bne 1b
+2:
+    bx lr
